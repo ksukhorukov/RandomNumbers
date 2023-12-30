7bd3ca067d209b23e41acda687ad58c662581649
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-12-31 07:02:32 +0800

    increment method for hours

--- manual_method_2.rb
+++ manual_method_2.rb
@@ -4,13 +4,27 @@ require 'pry'
 
 N = 1
 
+def padding(n)
+  n = n.to_s.split('')
+
+  size = n.size
+
+  n = n.join('')
+  binding.pry
+  return '0' + n if size < 2 
+
+  return n
+end
+
 def increment_hours_by(timestamp = '000000', n)
   data = timestamp.split('').map(&:to_i)
-  # binding.pry
+
   increment = ((data[0, 2].join.to_i + n) % 60).to_s
+
   slice = data[2,4].join('')
-  result = increment + slice
-  # binding.pry
+
+  result = padding(increment) + slice
+
   return result
 end 
 
